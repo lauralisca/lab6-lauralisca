cb16e19530d02846e57bf823f7f4bc544559d241
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-01 13:20:37 -0500

    lab 6

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,15 @@
-# Write your code here!
+def employee_print(employee_info):
+    info = employee_info.copy()
+    name = info.pop("Name", "N/A")
+    salary = info.pop ("Salary", "N/A")
+    role = info.pop ("Role", "N/A")
+
+    print (f"Name: {name}")
+    print (f"Salary: {salary}")
+    print (f"Role: {role}")
+
+    if len(info) == 0:
+        print ("No other info!")
+    else:
+        for key, value in info.items():
+            print(f"{key}: {value}")
