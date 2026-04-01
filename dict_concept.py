8970f72f9ce5241463ac702b9e30abc95e28a9ee
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-01 13:00:27 -0500

    lab 6

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,8 @@
-# Write your code here!
+def get_config():
+    config ={
+        "width": 480,
+        "height": 480,
+        "color_mode": "dark",
+        "sensitivity": 0.4
+    }
+    return config
