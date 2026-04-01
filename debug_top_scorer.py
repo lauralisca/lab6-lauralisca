155d0608fef8df5a70b121828ef7be0dbad0c12c
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-01 16:24:26 -0500

    lab 6

--- debug_top_scorer.py
+++ debug_top_scorer.py
@@ -1,4 +1,4 @@
-scores = []
+scores = {}
 
 while True:
     user_input = input("Enter player and score as 'name score' (or type 'stop' to finish):\n")
@@ -8,7 +8,7 @@ while True:
     name, score = user_input.split()
     score = int(score)
     if name in scores:
-        scores[name] -= score
+        scores[name] += score
     else:
         scores[name] = score
 
@@ -16,9 +16,9 @@ if len(scores) == 0:
     print("No scores recorded.")
 else:
     top_name = ""
-    top_score = 0
+    top_score = None 
     for name in scores:
-        if scores[name] < top_score:
+        if top_score is None or scores [name] > top_score:
             top_score = scores[name]
             top_name = name
     print(f"Top scorer: {top_name} with {top_score} points.")
