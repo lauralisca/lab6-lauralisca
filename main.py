ad00cf2fe63dc56c0026853dd901158aa1bc3ebb
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-04-01 16:17:14 -0500

    lab 5

---
+++ main.py
@@ -0,0 +1,42 @@
+from grades_manager import *
+
+def main():
+    print ("Welcome to the Student Grades Manager!\n")
+    my_grades = {}
+    while True:
+        print("Select an option:")
+        print("1. Add a student")
+        print("2. Print student grade averages")
+        print("3. Exit")
+        option = input().strip()
+
+        if option == "1":
+            my_grades = add_student(my_grades)
+        elif option == "2":
+            print ("Select an option:")
+            print ("a. Display all students")
+            print ("b. Display selected students")
+            sub_option = input().strip().lower()
+
+            if  sub_option == "b":
+                names = input(
+                    "Enter student names (comma-separated):\n"
+                )
+                names_list = [n.strip() for n in names.split(",")]
+                avg_by_student(my_grades, names_list)
+            else:
+                print("Invalid option selected!")
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+           
+
+
+
+           
